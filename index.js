eb99545e7ddf6e0055049338777d4caafbd72dba
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2024-05-21 12:25:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const navEls = document.querySelectorAll(".nav");
+
+openBtn.addEventListener("click", () => {
+  navEls.forEach((nav) => {
+    nav.classList.add("visible");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  navEls.forEach((nav) => {
+    nav.classList.remove("visible");
+  });
+});
